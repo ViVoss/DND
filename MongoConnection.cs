eb034d61540f321f49309d1b247cfe7e3faa3e6e
Jonin369 <44468329+Jonin369@users.noreply.github.com>
2020-03-05 09:32:10 +0100

    Added new MongoConnection

---
+++ MongoConnection.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DND
+{
+    class MongoConnection
+    {
+        public static IMongoClient Client { get; set; }
+        public static IMongoDatabase Database { get; set; }
+        public static string ConnectionString { get; set; } = "mongodb+srv://dbAdmin:Passwort123@dnd-wqlyc.mongodb.net/test?retryWrites=true&w=majority";
+        public static string DatabaseString { get; set; } = "DnD";
+
+        internal static void Connect()
+        {
+            try
+            {
+                Client = new MongoClient(ConnectionString);
+                Database = Client.GetDatabase(DatabaseString);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
