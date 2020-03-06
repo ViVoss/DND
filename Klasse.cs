5618f41a8e1a55aa75b28fff3b08fb7011ec6e09
Philipp Pohland <p.pohland@web.de>
2020-03-06 12:02:56 +0100

    KlassenFETCH ALLA

---
+++ Klasse.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DND
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class Klasse
+    {
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("index")]
+        public string Index { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("hit_die")]
+        public long HitDie { get; set; }
+
+        [JsonProperty("proficiency_choices")]
+        public List<ProficiencyChoice> ProficiencyChoices { get; set; }
+
+        [JsonProperty("proficiencies")]
+        public List<Proficiency> Proficiencies { get; set; }
+
+        [JsonProperty("saving_throws")]
+        public List<Proficiency> SavingThrows { get; set; }
+
+        [JsonProperty("starting_equipment")]
+        public ClassLevels StartingEquipment { get; set; }
+
+        [JsonProperty("class_levels")]
+        public ClassLevels ClassLevels { get; set; }
+
+        [JsonProperty("subclasses")]
+        public List<Proficiency> Subclasses { get; set; }
+
+        [JsonProperty("spellcasting")]
+        public ClassLevels Spellcasting { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+
+    public partial class ClassLevels
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("class")]
+        public string Class { get; set; }
+    }
+
+    public partial class Proficiency
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+
+    public partial class ProficiencyChoice
+    {
+        [JsonProperty("choose")]
+        public long Choose { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("from")]
+        public List<Proficiency> From { get; set; }
+    }
+    public partial class Klasse
+    {
+        public static Klasse FromJson(string json) => JsonConvert.DeserializeObject<Klasse>(json, DND.Converter.Settings);
+    }
+}
---
+++ WebAufruf.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+
+namespace DND
+{
+    public class WebAufruf
+    {
+        public string GetRaceInformation(string race)
+        {
+            string jsonstring;
+            System.Text.StringBuilder builder = new System.Text.StringBuilder();
+            string url = "http://dnd5eapi.co/api/races/rasse";
+            url = url.Replace("rasse", race);
+
+            //Creates Request of URl
+            WebRequest request = WebRequest.Create(url);
+
+            //get Response
+            WebResponse response = request.GetResponse();
+
+            using (Stream dataStream = response.GetResponseStream())
+            {
+
+                StreamReader reader = new StreamReader(dataStream);
+
+                // Hier komm der JSon String
+                jsonstring = reader.ReadToEnd();
+
+                Rasse rasse = JsonConvert.DeserializeObject<Rasse>(jsonstring);
+
+                builder.Append(rasse.Name);
+                foreach (DND.AbilityBonus element in rasse.AbilityBonuses)
+                {
+                    builder.Append("\n" + element.Name + " +" + element.Bonus);
+                }
+            }
+            response.Close();
+            return builder.ToString();
+        }
+        public string GetClassInformation(string klasse)
+        {
+            string jsonstring;
+            System.Text.StringBuilder builder = new System.Text.StringBuilder();
+            string url = "http://dnd5eapi.co/api/classes/klasse";
+            url = url.Replace("klasse", klasse);
+
+            //Creates Request of URl
+            WebRequest request = WebRequest.Create(url);
+
+            //get Response
+            WebResponse response = request.GetResponse();
+
+            using (Stream dataStream = response.GetResponseStream())
+            {
+
+                StreamReader reader = new StreamReader(dataStream);
+
+                // Hier komm der JSon String
+                jsonstring = reader.ReadToEnd();
+
+                Klasse clazz = JsonConvert.DeserializeObject<Klasse>(jsonstring);
+                builder.Append("HitDice 1d" + clazz.HitDie + " pro " + klasse + " level" + "\n");
+                builder.Append("Hitpoints at 1st Level " + clazz.HitDie + " + Konstitution Modifikator" + "\n");
+                builder.Append("Hitpoints at Higher Level " + "1d" + clazz.HitDie + "(or" + ((clazz.HitDie / 2) + 1) + ")" + "\n");
+                foreach (DND.Proficiency prof in clazz.Proficiencies)
+                {
+                    builder.Append(prof.Name + "\n");
+                }
+            }
+            response.Close();
+            return builder.ToString();
+        }
+    }    
+}
