404b07a293a6b0e2e09a19ec9e9eb81200b47990
OPE24 <61269123+OPE24@users.noreply.github.com>
2020-03-12 10:32:14 +0100

    Att.start

--- Creation/Page_AttributeSelection.xaml.cs
+++ Creation/Page_AttributeSelection.xaml.cs
@@ -41,34 +41,40 @@ namespace DND
 
         private void cmbAttributeValuesStr_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string attval = "";
-            switch (((ComboBox)sender).SelectedIndex)
-            {
-                case 0:
-                    attval = Convert.ToString(-1);
-                    break;
-                case 1:
-                    attval = Convert.ToString(0);
-                    break;
-                case 2:
-                    attval = Convert.ToString("+" + 1);
-                    break;
-                case 3:
-                    attval = Convert.ToString("+" + 2);
-                    break;
-                case 4:
-                    attval = Convert.ToString("+" + 3);
-                    break;
-                case 5:
-                    attval = Convert.ToString("+" + 4);
-                    break;
-                case 6:
-                    attval = Convert.ToString("+" + 5);
-                    break;
-                default:
-                    break;
-            }
-            Label_STR.Content = attval;
+            Character.Current.Attributes.Strength = ((ComboBox) sender).SelectedValue;
+            this.Label_STR.Text = Character.Current.Attributes.StrengthModifier;
+            
+
+
+
+            //string attval = "";
+            //switch (((ComboBox)sender).SelectedIndex)
+            //{
+            //    case 0:
+            //        attval = Convert.ToString(-1);
+            //        break;
+            //    case 1:
+            //        attval = Convert.ToString(0);
+            //        break;
+            //    case 2:
+            //        attval = Convert.ToString("+" + 1);
+            //        break;
+            //    case 3:
+            //        attval = Convert.ToString("+" + 2);
+            //        break;
+            //    case 4:
+            //        attval = Convert.ToString("+" + 3);
+            //        break;
+            //    case 5:
+            //        attval = Convert.ToString("+" + 4);
+            //        break;
+            //    case 6:
+            //        attval = Convert.ToString("+" + 5);
+            //        break;
+            //    default:
+            //        break;
+            //}
+            //Label_STR.Content = attval;
         }
     }
 }
