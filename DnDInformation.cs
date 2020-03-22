70f39fb7c7eed025ff3aa84bf9c41318baf89a52
Marvko94 <marvin.rosek94@gmail.com>
2020-03-22 19:06:34 +0100

    Add method GetSubRaceDbInformation, GetBackgroundInformation and change GetClassInformation

--- DnDInformation.cs
+++ DnDInformation.cs
@@ -11,6 +11,7 @@ namespace DND
     {
         StringBuilder builder = new StringBuilder();
         private string requestParameter;
+        private int index;
         public string GetRaceInformation(string rasse)
         {
             builder.Clear();
@@ -95,19 +96,44 @@ namespace DND
             return builder.ToString();
         }
 
+        public string GetSubraceDbInformation(string subrasse)
+        {
+            builder.Clear();
+
+            MongoConnection<SubRace> mongoConnection = new MongoConnection<SubRace>();
+            SubRace subrace = mongoConnection.GetDocumentByIndex("Subrace", subrasse);
+
+            builder.Append(subrace.Name);
+
+            return builder.ToString();
+        }
+
         public string GetClassInformation(string klasse)
         {
             builder.Clear();
             WebAufruf<Klasse> webAufruf = new WebAufruf<Klasse>();
+            WebAufruf<StartingEquipment> webAufruf1 = new WebAufruf<StartingEquipment>();
+
             requestParameter = "classes/" + klasse;
             Klasse clazz = webAufruf.GetJsonResponse(requestParameter);
 
-            builder.Append("Hit Points:" + "\n\n");
-            builder.Append("Hit Dice: 1d" + clazz.HitDie + " per " + klasse + " level\n\n");
-            builder.Append("Hit Points at 1st Level: " + clazz.HitDie + " + your Constitution modifier\n\n");
-            builder.Append("Hit Points at Higher Levels: " + "1d" + clazz.HitDie + " (or " + ((clazz.HitDie / 2) + 1) + ") + your Constitution modifier per " + klasse + " lever after 1st\n\n");
+            foreach (int enumVal in Enum.GetValues(typeof(EnumKlasse)))
+            {
+                if (klasse.Equals(Enum.GetName(typeof(EnumKlasse), enumVal)))
+                {
+                    index = enumVal;
+                }
+            }
 
-            builder.Append("Proficiencies: ");
+            requestParameter = "starting-equipment/" + index;
+            StartingEquipment startingEquipment = webAufruf1.GetJsonResponse(requestParameter);
+
+            builder.Append("Hit Points: ");
+            builder.Append("\n\nHit Dice: 1d" + clazz.HitDie + " per " + klasse + " level.");
+            builder.Append("\n\nHit Points at 1st Level: " + clazz.HitDie + " + your constitution modifier.");
+            builder.Append("\n\nHit Points at Higher Levels: " + "1d" + clazz.HitDie + " (or " + ((clazz.HitDie / 2) + 1) + ") + your Constitution modifier per " + klasse + " lever after 1st.");
+
+            builder.Append("\n\nProficiencies: ");
             foreach (Proficiency prof in clazz.Proficiencies)
             {
                 if (prof.Equals(clazz.Proficiencies.Last()))
@@ -152,12 +178,244 @@ namespace DND
                 }
             }
 
+            builder.Append("\n\nStarting Equipment: ");
+            foreach (StartingEquipmentElement startingEquipmentElement in startingEquipment.StartingEquipmentElement)
+            {
+                if (startingEquipmentElement.Equals(startingEquipment.StartingEquipmentElement.Last()))
+                {
+                    builder.Append(startingEquipmentElement.Quantity + " " + startingEquipmentElement.Item.Name);
+                }
+                else
+                {
+                    builder.Append(startingEquipmentElement.Quantity + " " + startingEquipmentElement.Item.Name + ", ");
+                }
+            }
 
-            builder.Append(clazz.StartingEquipment);
+            builder.Append("\n\nChoices to make: " + startingEquipment.ChoicesToMake);
+
+            if (startingEquipment.GetType().GetProperty("Choice") != null)
+            {
+                builder.Append("\n\nChoice 1:");
+                foreach (Choice choice in startingEquipment.Choice ?? new List<Choice>())
+                {
+                    builder.Append("\nChoose " + choice.Choose + " from: ");
+                    foreach (StartingEquipmentElement from in choice.From)
+                    {
+                        if (from.Equals(choice.From.Last()))
+                        {
+                            builder.Append(from.Quantity + " " + from.Item.Name);
+                        }
+                        else
+                        {
+                            builder.Append(from.Quantity + " " + from.Item.Name + ", ");
+                        }
+                    }
+                }
+            }
+
+            
+            if (startingEquipment.GetType().GetProperty("Choice2") != null)
+            {
+                builder.Append("\n\nChoice 2:");
+                foreach (Choice choice in startingEquipment.Choice2 ?? new List<Choice>())
+                {
+                    builder.Append("\nChoose " + choice.Choose + " from: ");
+                    foreach (StartingEquipmentElement from in choice.From)
+                    {
+                        if (from.Equals(choice.From.Last()))
+                        {
+                            builder.Append(from.Quantity + " " + from.Item.Name);
+                        }
+                        else
+                        {
+                            builder.Append(from.Quantity + " " + from.Item.Name + ", ");
+                        }
+                    }
+                }
+            }
+
+            if (startingEquipment.GetType().GetProperty("Choice3") != null)
+            {
+                builder.Append("\n\nChoice 3:");
+                foreach (Choice choice in startingEquipment.Choice3 ?? new List<Choice>())
+                {
+                    builder.Append("\nChoose " + choice.Choose + " from: ");
+                    foreach (StartingEquipmentElement from in choice.From)
+                    {
+                        if (from.Equals(choice.From.Last()))
+                        {
+                            builder.Append(from.Quantity + " " + from.Item.Name);
+                        }
+                        else
+                        {
+                            builder.Append(from.Quantity + " " + from.Item.Name + ", ");
+                        }
+                    }
+                }
+            }
+
+            if (startingEquipment.GetType().GetProperty("Choice4") != null)
+            {
+                builder.Append("\n\nChoice 4:");
+                foreach (Choice choice in startingEquipment.Choice4 ?? new List<Choice>())
+                {
+                    builder.Append("\nChoose " + choice.Choose + " from: ");
+                    foreach (StartingEquipmentElement from in choice.From)
+                    {
+                        if (from.Equals(choice.From.Last()))
+                        {
+                            builder.Append(from.Quantity + " " + from.Item.Name);
+                        }
+                        else
+                        {
+                            builder.Append(from.Quantity + " " + from.Item.Name + ", ");
+                        }
+                    }
+                }
+            }
+
+            if (startingEquipment.GetType().GetProperty("Choice5") != null)
+            {
+                builder.Append("\n\nChoice 5:");
+                foreach (Choice choice in startingEquipment.Choice5 ?? new List<Choice>())
+                {
+                    builder.Append("\nChoose " + choice.Choose + " from: ");
+                    foreach (StartingEquipmentElement from in choice.From)
+                    {
+                        if (from.Equals(choice.From.Last()))
+                        {
+                            builder.Append(from.Quantity + " " + from.Item.Name);
+                        }
+                        else
+                        {
+                            builder.Append(from.Quantity + " " + from.Item.Name + ", ");
+                        }
+                    }
+                }
+            }
 
             return builder.ToString();
         }
 
-        //public String BackgroundInformation
+        public string GetBackgroundInformation(string hintergrund)
+        {
+            builder.Clear();
+            MongoConnection<Background> mongoConnection = new MongoConnection<Background>();
+            Background background = mongoConnection.GetDocumentByIndex("Background", hintergrund);
+
+            if (background.SkillProficiences.Count() > 0)
+            {
+                builder.Append("Skill Proficiences: ");
+                foreach (string prof in background.SkillProficiences)
+                {
+                    if (prof.Equals(background.SkillProficiences.Last()))
+                    {
+                        builder.Append(prof);
+                    }
+                    else
+                    {
+                        builder.Append(prof + ", ");
+                    }
+                }
+            }
+
+            if (background.ToolProficiences.Count() > 0)
+            {
+                builder.Append("\n\nTool Proficiences: ");
+                foreach (string prof in background.ToolProficiences)
+                {
+                    if (prof.Equals(background.ToolProficiences.Last()))
+                    {
+                        builder.Append(prof);
+                    }
+                    else
+                    {
+                        builder.Append(prof + ", ");
+                    }
+                }
+            }
+
+            if (background.Languages.Count() > 0)
+            {
+                builder.Append("\n\nLanguages: " + background.Languages);
+                foreach (string lang in background.Languages)
+                {
+                    if (lang.Equals(background.Languages.Last()))
+                    {
+                        builder.Append(lang);
+                    }
+                    else
+                    {
+                        builder.Append(lang + ", ");
+                    }
+                }
+            }
+
+            if (background.LanguageOptions.Choose > 0)
+            {
+                builder.Append("\n\nLanguage Options: Choose " + background.LanguageOptions.Choose + " from: ");
+                foreach (string lang in background.LanguageOptions.From)
+                {
+                    if (lang.Equals(background.Equipment.Last()))
+                    {
+                        builder.Append(lang);
+                    }
+                    else
+                    {
+                        builder.Append(lang + ", ");
+                    }
+                }
+            }
+
+            if (background.Equipment.Count() > 0)
+            {
+                builder.Append("\n\nEquipment: ");
+                foreach (string equip in background.Equipment)
+                {
+                    if (equip.Equals(background.Equipment.Last()))
+                    {
+                        builder.Append(equip);
+                    }
+                    else
+                    {
+                        builder.Append(equip + ", ");
+                    }
+                }
+            }
+
+            if (background.EquipmentOptions.Choose > 0)
+            {
+                builder.Append("\n\nEquipment Options: Choose " + background.EquipmentOptions.Choose + " from: ");
+                foreach (string equip in background.EquipmentOptions.From)
+                {
+                    if (equip.Equals(background.Equipment.Last()))
+                    {
+                        builder.Append(equip);
+                    }
+                    else
+                    {
+                        builder.Append(equip + ", ");
+                    }
+                }
+            }
+
+            if (background.Feature.Count() > 0)
+            {
+                builder.Append("\n\nFeature: ");
+                foreach (string feat in background.Feature)
+                {
+                    if (feat.Equals(background.Feature.Last()))
+                    {
+                        builder.Append(feat);
+                    }
+                    else
+                    {
+                        builder.Append(feat + ", ");
+                    }
+                }
+            }
+
+            return builder.ToString();
+        }
     }
 }
