9dc9de5ece11773f2a978e1874b3469cfd7a36ba
Marvko94 <marvin.rosek94@gmail.com>
2020-03-22 19:05:23 +0100

    Add new classes

---
+++ EnumKlasse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DND
+{
+    enum EnumKlasse
+    {
+        barbarian = 1,
+        bard = 2,
+        cleric = 3,
+        druid = 4,
+        fighter = 5,
+        monk = 6,
+        paladin = 7,
+        ranger = 8,
+        rogue = 9,
+        sorcerer = 10,
+        warlock = 11,
+        wizard = 12
+    }
+}
