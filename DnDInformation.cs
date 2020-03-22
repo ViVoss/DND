5c08dbce213ac94289ce8832ea78f803c45ad55b
Marvko94 <marvin.rosek94@gmail.com>
2020-03-22 13:41:43 +0100

    Split WebAufruf into new class

---
+++ DnDInformation.cs
@@ -0,0 +1,163 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DND
+{
+    public class DnDInformation
+    {
+        StringBuilder builder = new StringBuilder();
+        private string requestParameter;
+        public string GetRaceInformation(string rasse)
+        {
+            builder.Clear();
+            WebAufruf<Rasse> webAufruf = new WebAufruf<Rasse>();
+            requestParameter = "races/" + rasse;
+            Rasse race = webAufruf.GetJsonResponse(requestParameter);
+
+            builder.Append("Ability Score Increase: ");
+            foreach (AbilityBonus abilityBonus in race.AbilityBonuses)
+            {
+                if (abilityBonus.Equals(race.AbilityBonuses.Last()))
+                {
+                    builder.Append(abilityBonus.Name + " + " + abilityBonus.Bonus);
+                }
+                else
+                {
+                    builder.Append(abilityBonus.Name + ", ");
+                }
+            }
+
+            builder.Append("\n\nAge: " + race.Age);
+            builder.Append("\n\nAlignment: " + race.Alignment);
+            builder.Append("\n\nSize: " + race.SizeDescription);
+
+            builder.Append("\n\nTraits: ");
+            foreach (Trait trait in race.Traits)
+            {
+                if (trait.Equals(race.Traits.Last()))
+                {
+                    builder.Append(trait.Name);
+                }
+                else
+                {
+                    builder.Append(trait.Name + ", ");
+                }
+            }
+
+            builder.Append("\n\nSpeed: " + race.Speed + "ft.");
+
+            builder.Append("\n\nLanguages: ");
+            foreach (Language language in race.Languages)
+            {
+                if (language.Equals(race.Languages.Last()))
+                {
+                    builder.Append(language.Name);
+                }
+                else
+                {
+                    builder.Append(language.Name + ", ");
+                }
+            }
+
+            builder.Append("\n\nSubraces: ");
+            foreach (SubRace subRace in race.Subraces)
+            {
+                if (subRace.Equals(race.Subraces.Last()))
+                {
+                    builder.Append(subRace.Name);
+                }
+                else
+                {
+                    builder.Append(subRace.Name + ", ");
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        public string GetSubRaceInformation(string subrasse)
+        {
+            builder.Clear();
+            WebAufruf<SubRace> webAufruf = new WebAufruf<SubRace>();
+            requestParameter = "subraces/" + subrasse;
+            SubRace subrace = webAufruf.GetJsonResponse(requestParameter);
+
+            builder.Append(subrace.Desc);
+            builder.Append("\n\nAbility Score Increase: " + subrace.AbilityBonuses);
+            builder.Append("\n\nStarting Proficiences: " + subrace.StartingProficiencies);
+            builder.Append("\n\nLanguages: " + subrace.Languages);
+            builder.Append("\n\nRacial Traits: " + subrace.RacialTraits);
+
+            return builder.ToString();
+        }
+
+        public string GetClassInformation(string klasse)
+        {
+            builder.Clear();
+            WebAufruf<Klasse> webAufruf = new WebAufruf<Klasse>();
+            requestParameter = "classes/" + klasse;
+            Klasse clazz = webAufruf.GetJsonResponse(requestParameter);
+
+            builder.Append("Hit Points:" + "\n\n");
+            builder.Append("Hit Dice: 1d" + clazz.HitDie + " per " + klasse + " level\n\n");
+            builder.Append("Hit Points at 1st Level: " + clazz.HitDie + " + your Constitution modifier\n\n");
+            builder.Append("Hit Points at Higher Levels: " + "1d" + clazz.HitDie + " (or " + ((clazz.HitDie / 2) + 1) + ") + your Constitution modifier per " + klasse + " lever after 1st\n\n");
+
+            builder.Append("Proficiencies: ");
+            foreach (Proficiency prof in clazz.Proficiencies)
+            {
+                if (prof.Equals(clazz.Proficiencies.Last()))
+                {
+                    builder.Append(prof.Name);
+                }
+                else
+                {
+                    builder.Append(prof.Name + ", ");
+                }
+            }
+
+            builder.Append("\n\nSaving Throws: ");
+            foreach (Proficiency saveThr in clazz.SavingThrows)
+            {
+                if (saveThr.Equals(clazz.SavingThrows.Last()))
+                {
+                    builder.Append(saveThr.Name);
+                }
+                else
+                {
+                    builder.Append(saveThr.Name + ", ");
+                }
+            }
+
+            foreach (ProficiencyChoice skills in clazz.ProficiencyChoices)
+            {
+                builder.Append("\n\nChoose " + skills.Choose + " from: ");
+                foreach (Proficiency prof in skills.From)
+                {
+                    string skillsStr = prof.Name;
+                    skillsStr = skillsStr.Replace("Skill: ", "");
+
+                    if (prof.Equals(skills.From.Last()))
+                    {
+                        builder.Append(skillsStr);
+                    }
+                    else
+                    {
+                        builder.Append(skillsStr + ", ");
+                    }
+                }
+            }
+
+
+            builder.Append(clazz.StartingEquipment);
+
+            return builder.ToString();
+        }
+
+        //public String BackgroundInformation
+    }
+}
--- WebAufruf.cs
+++ WebAufruf.cs
@@ -1,23 +1,28 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Cache;
 using System.Net.Http;
-using System.Text.RegularExpressions;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
-using System.Web.Script.Serialization;
-using Newtonsoft.Json;
-using System.IO;
-using System.Net;
-using System.Net.Cache;
-using System.Linq;
-using System.Text;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace DND
 {
-    public class WebAufruf
+    public class WebAufruf<T>
     {
-        StringBuilder builder = new StringBuilder();
+        // StringBuilder builder = new StringBuilder();
         private string jsonstring;
         private string uri;
 
@@ -27,10 +32,9 @@ namespace DND
             request.CachePolicy = caching;
         }
 
-        public string GetRaceInformation(string rasse)
+        public dynamic GetJsonResponse(string requestParameter)
         {
-            builder.Clear();
-            uri = "http://dnd5eapi.co/api/races/" + rasse;
+            uri = "http://dnd5eapi.co/api/" + requestParameter;
 
             // Create WebRequest to URI
             WebRequest request = WebRequest.Create(uri);
@@ -47,179 +51,10 @@ namespace DND
                 // Filter JSON String from Response
                 jsonstring = reader.ReadToEnd();
 
-                Rasse race = JsonConvert.DeserializeObject<Rasse>(jsonstring);
-                
-                builder.Append("Ability Score Increase: ");
-                foreach (AbilityBonus abilityBonus in race.AbilityBonuses)
-                {
-                    if (abilityBonus.Equals(race.AbilityBonuses.Last()))
-                    {
-                        builder.Append(abilityBonus.Name + " + " + abilityBonus.Bonus);
-                    }
-                    else
-                    {
-                        builder.Append(abilityBonus.Name + ", ");
-                    }
-                }
+                // response.Close();
 
-                builder.Append("\n\nAge: " + race.Age);
-                builder.Append("\n\nAlignment: " + race.Alignment);
-                builder.Append("\n\nSize: " + race.SizeDescription);
- 
-                builder.Append("\n\nTraits: ");
-                foreach (Trait trait in race.Traits)
-                {
-                    if (trait.Equals(race.Traits.Last()))
-                    {
-                        builder.Append(trait.Name);
-                    }
-                    else
-                    {
-                        builder.Append(trait.Name + ", ");
-                    }
-                }
-
-                builder.Append("\n\nSpeed: " + race.Speed + "ft.");
-                
-                builder.Append("\n\nLanguages: ");
-                foreach (Language language in race.Languages)
-                {
-                    if (language.Equals(race.Languages.Last()))
-                    {
-                        builder.Append(language.Name);
-                    }
-                    else
-                    {
-                        builder.Append(language.Name + ", ");
-                    }
-                }
-
-                builder.Append("\n\nSubraces: ");
-                foreach (SubRace subRace in race.Subraces)
-                {
-                    if (subRace.Equals(race.Subraces.Last()))
-                    {
-                        builder.Append(subRace.Name);
-                    }
-                    else
-                    {
-                        builder.Append(subRace.Name + ", ");
-                    }
-                }
-
+                return JsonConvert.DeserializeObject<T>(jsonstring);
             }
-            response.Close();
-            return builder.ToString();
-        }
-        public string GetSubRaceInformation(string subrasse)
-        {
-            builder.Clear();
-            uri = "http://dnd5eapi.co/api/subraces/" + subrasse;
-
-            // Create WebRequest of URI
-            WebRequest request = WebRequest.Create(uri);
-            SetCache(request);
-
-            // Get Response from WebRequest
-            WebResponse response = request.GetResponse();
-
-            using (Stream dataStream = response.GetResponseStream())
-            {
-
-                StreamReader reader = new StreamReader(dataStream);
-
-                // Filter JSON String from Response
-                jsonstring = reader.ReadToEnd();
-
-                SubRace subrace = JsonConvert.DeserializeObject<SubRace>(jsonstring);
-
-                builder.Append(subrace.Desc);
-                builder.Append("\n\nAbility Score Increase: " + subrace.AbilityBonuses);
-                builder.Append("\n\nStarting Proficiences: " + subrace.StartingProficiencies);
-                builder.Append("\n\nLanguages: " + subrace.Languages);
-                builder.Append("\n\nRacial Traits: " + subrace.RacialTraits);
-            }
-            response.Close();
-            return builder.ToString();
-        }
-
-        public string GetClassInformation(string klasse)
-        {
-            builder.Clear();
-            uri = "http://dnd5eapi.co/api/classes/" + klasse;
-
-            // Create WebRequest of URI
-            WebRequest request = WebRequest.Create(uri);
-            SetCache(request);
-
-            // Get Response from WebRequest
-            WebResponse response = request.GetResponse();
-
-            using (Stream dataStream = response.GetResponseStream())
-            {
-
-                StreamReader reader = new StreamReader(dataStream);
-
-                // Filter JSON String from Response
-                jsonstring = reader.ReadToEnd();
-
-                Klasse clazz = JsonConvert.DeserializeObject<Klasse>(jsonstring);
-
-                builder.Append("Hit Points:" + "\n\n");
-                builder.Append("Hit Dice: 1d" + clazz.HitDie + " per " + klasse + " level\n\n");
-                builder.Append("Hit Points at 1st Level: " + clazz.HitDie + " + your Constitution modifier\n\n");
-                builder.Append("Hit Points at Higher Levels: " + "1d" + clazz.HitDie + " (or " + ((clazz.HitDie / 2) + 1) + ") + your Constitution modifier per " + klasse + " lever after 1st\n\n");
-     
-                builder.Append("Proficiencies: ");
-                foreach (Proficiency prof in clazz.Proficiencies)
-                {
-                    if (prof.Equals(clazz.Proficiencies.Last()))
-                    {
-                        builder.Append(prof.Name);
-                    }
-                    else
-                    {
-                        builder.Append(prof.Name + ", ");
-                    }
-                }
-
-                builder.Append("\n\nSaving Throws: ");
-                foreach (Proficiency saveThr in clazz.SavingThrows)
-                {
-                    if (saveThr.Equals(clazz.SavingThrows.Last()))
-                    {
-                        builder.Append(saveThr.Name);
-                    }
-                    else
-                    {
-                        builder.Append(saveThr.Name + ", ");
-                    }
-                }
-
-                foreach (ProficiencyChoice skills in clazz.ProficiencyChoices)
-                {
-                    builder.Append("\n\nChoose " + skills.Choose + " from: ");
-                    foreach (Proficiency prof in skills.From)
-                    {
-                        string skillsStr = prof.Name;
-                        skillsStr = skillsStr.Replace("Skill: ", "");
-
-                        if (prof.Equals(skills.From.Last()))
-                        {
-                            builder.Append(skillsStr);
-                        }
-                        else
-                        {
-                            builder.Append(skillsStr + ", ");
-                        }
-                    }
-                }
-
-                builder.Append(clazz.StartingEquipment);
-            }
-
-            response.Close();
-            return builder.ToString();
         }
     }
 }
